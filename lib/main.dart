e066801b0f0ed1898a2acb911c5f249ccbbfdc3c
Sanzhar <s.kalibekov54@gmail.com>
2023-04-12 23:27:32 +0600

    SU page init

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,91 @@
+// импортируйте необходимые библиотеки
+import 'package:chatter/Server/ServerConfig.dart';
+import 'package:chatter/pages/Authority/LoginPage.dart';
+import 'package:chatter/pages/Authority/RegisterPage.dart';
+import 'package:chatter/pages/MainPage.dart';
+import 'package:chatter/screens/home_screens.dart';
+import 'package:chatter/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool hasAccessToken = false;
+  ServerConfig serverConfig = ServerConfig();
+  Future<void> checkToken() async {
+    const storage = FlutterSecureStorage();
+    final String? token = await storage.read(key: 'accessToken');
+
+    final response = await http.get(
+      Uri.parse('${ServerConfig.ip}/api/auth/check'),
+      headers: <String, String>{
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // если токен доступа есть, устанавливаем hasAccessToken в true
+      setState(() {
+        hasAccessToken = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // проверьте наличие токена доступа при запуске приложения
+    checkToken();
+  }
+
+  Future<void> loginSuccess(var state) async {
+    setState(() {
+      hasAccessToken = state == 1 ? true : false;
+    });
+
+    runApp(MyApp());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chatter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+    if (hasAccessToken) {
+      // если у нас есть токен, открываем страницу Pages
+      return MaterialApp(
+        theme: AppTheme.dark(),
+        title: 'Elhagram',
+        themeMode: ThemeMode.dark,
+        home: HomeScreen(
+          loginSuccess: loginSuccess,
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+      );
+    }
+
+
+    else {
+      // если у нас нет токена, открываем страницу входа
+      return MaterialApp(
+        title: 'Elhagram',
+        theme: AppTheme.dark(),
+        themeMode: ThemeMode.dark,
+        routes: {
+          '/Login': (context) => LoginPage(loginSuccess: loginSuccess),
+          '/Register': (context) => RegisterPage(loginSuccess: loginSuccess),
+        },
+        home: const MainPage(),
+      );
+    }
   }
 }
